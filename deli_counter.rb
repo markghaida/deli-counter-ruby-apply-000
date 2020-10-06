5fa6aa0c60bcb23b545095d3b0aa97577292b213
Mark G <markghaida@gmail.com>
2020-10-06 18:49:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def take_a_number (katz_deli)
   
   katz_counter = 0
   katz_counter += 1
-  katz_deli << 
+  katz_deli << katz_counter
   puts "Welcome, you are ticket number #{katz_deli.length} in line."
 end
 
