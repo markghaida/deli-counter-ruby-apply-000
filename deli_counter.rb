d99b5fc2d63bedade90289e367e17de37ab63262
Mark G <markghaida@gmail.com>
2020-10-06 18:48:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def take_a_number (katz_deli)
   
-  # how to make this start previous iteration katz_counter = 0
+  katz_counter = 0
   katz_counter += 1
   katz_deli << 
   puts "Welcome, you are ticket number #{katz_deli.length} in line."
