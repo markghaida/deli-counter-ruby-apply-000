9104cf1ff0c829aaaa9837ec6e1ed21bb6103e2c
Mark G <markghaida@gmail.com>
2020-10-06 16:31:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,55 +1,42 @@
 # Write your code here.
 
-# katz_deli = [Logan, Avi, Spencer]
-
- def line (katz_deli)
-   
-   if katz_deli.length > 0 
-       
-       index = 0 
-       counter = 1
-       empty_array = []
-       
-       while index < katz_deli.length 
-       
-         empty_array.push(" #{counter}. #{katz_deli[index]}")
-         
-         counter += 1 
-         index += 1 
-           
-       end 
-         
-      
-      puts "The line is currently:#{empty_array.join("")}"
-
-
-    else 
-      
-      puts "The line is currently empty."
-      
-  end 
-end  
-    
-    
-def take_a_number (katz_deli, caboose_name)
-  
- katz_deli << caboose_name
-  
-  puts "Welcome, #{caboose_name}. You are number #{katz_deli.length} in line."
-  
+def line (katz_deli)
+   if katz_deli.length == 0
+     puts "The line is currently empty."
+   else
+     index = 0
+     new_array = [] 
+     
+       while index < katz_deli.length
+         new_array.push("#{index + 1}. #{katz_deli[index]}")
+         #.each_with_index enumerator
+         index += 1
+       end
+    puts "The line is currently: #{new_array.join(" ")}" #cleanly forms all strings into a single line ($globalvariable)
+   end
 end
 
+
+def take_a_number (katz_deli, caboose_name)
+  katz_deli << caboose_name
+  puts "Welcome, #{caboose_name}. You are number #{katz_deli.length} in line."
+end
+
+def take_a_number (katz_deli)
+  
+  katz_counter = 0
+  katz_counter += 1
+  katz_deli << katz_counter
+  puts "You are number #{katz_deli} in line."
+end
+
+
+
 def now_serving (katz_deli)
-  
-  if katz_deli.length == 0 
-    
-    puts "There is nobody waiting to be served!"
-    
-  else 
-    
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
-    
-  end  
-  
+    if katz_deli.length == 0 
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{katz_deli[0]}."
+      katz_deli.shift #removes from front of line
+    end
 end
